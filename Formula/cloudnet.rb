0a31de249ac5c792f022c9c94ec3503845de040a
Jocker <yutaoo1230@gmail.com>
2023-09-22 19:53:29 +0800

    update homebrew-tap

--- Formula/cloudnet.rb
+++ Formula/cloudnet.rb
@@ -1,7 +1,7 @@
 class Cloudnet < Formula
   desc "A smart cloud network system!"
   homepage "https://cloudnet.world/kb/docs/"
-  version "1.36.2"
+  version "1.36.2.2"
   license "MIT"
 
   depends_on "curl" => :recommended
@@ -11,8 +11,8 @@ class Cloudnet < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://pkgs.cloudnet.world/stable/macos/cloudnet_1.36.2_amd64.zip"
-      sha256 "0a6564ef193fba0b02f40d9be5c848d1e4629980beaaed546940df5341bf5374"
+      url "https://pkgs.cloudnet.world/stable/macos/cloudnet_1.36.2.2_amd64.zip"
+      sha256 "8228360a0703c18fe31a7b7bb8ae8ecded73d23f7b76479923d7ac61c2cf82b8"
 
       def install
         bin.install "cnet"
@@ -23,8 +23,8 @@ class Cloudnet < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://pkgs.cloudnet.world/stable/macos/cloudnet_1.36.2_arm64.zip"
-      sha256 "985a4f1dede88c5a7b4c1c46d779eecf89e35bdae4e1ec357b3c82669738c371"
+      url "https://pkgs.cloudnet.world/stable/macos/cloudnet_1.36.2.2_arm64.zip"
+      sha256 "0355942f79f96b9bffd267699ad302c4187d174668b0293063d44b150a0aa071"
 
       def install
         bin.install "cnet"
@@ -34,12 +34,12 @@ class Cloudnet < Formula
     end
   end
 
-  service do
-    run [opt_bin / "cloudnetd"]
-    keep_alive true
-    log_path var / "log/cloudnetd.log"
-    error_log_path var / "log/cloudnetd.log"
-  end
+  # service do
+  #   run [opt_bin / "cloudnetd"]
+  #   keep_alive true
+  #   log_path var / "log/cloudnetd.log"
+  #   error_log_path var / "log/cloudnetd.log"
+  # end
 
   test do
     system "#{bin}/cnet version"
