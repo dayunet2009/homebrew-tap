1b8bafc52d72952634dcb462e49392ee6e30f7d7
cloudnet <support@cloudnet.world>
2025-04-25 00:19:59 +0800

    update homebrew-tap

--- Formula/cloudnet.rb
+++ Formula/cloudnet.rb
@@ -1,7 +1,7 @@
 class Cloudnet < Formula
   desc "A smart cloud network system!"
   homepage "https://cloudnet.world/kb/docs/"
-  version "1.36.2.12"
+  version "1.36.2.13"
   license "MIT"
 
   depends_on "curl" => :recommended
@@ -11,8 +11,8 @@ class Cloudnet < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://pkgs.cloudnet.world/stable/macos/cloudnet_1.36.2.12_amd64.zip"
-      sha256 "4c772f2722fc9bc6022a44534d9f555520eb2a1f7b7252ec255f2f424ff813d7"
+      url "https://pkgs.cloudnet.world/stable/macos/cloudnet_1.36.2.13_amd64.zip"
+      sha256 "181fd61a5ad9d234ba9ca864191c88fda44743e4f39c98b5c1d27573b45bfd8e"
 
       def install
         bin.install "cnet"
@@ -23,8 +23,8 @@ class Cloudnet < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://pkgs.cloudnet.world/stable/macos/cloudnet_1.36.2.12_arm64.zip"
-      sha256 "71b2d2162e553ea993f88837e07b916fa845e1a01e1c9a7de51ced7ed6aafcf3"
+      url "https://pkgs.cloudnet.world/stable/macos/cloudnet_1.36.2.13_arm64.zip"
+      sha256 "de3872516669295ba34780e1afc2e3b0eb3e9b20b9fe736889946d88f724ccdb"
 
       def install
         bin.install "cnet"
